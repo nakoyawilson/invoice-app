5b471cd187043b2d73f1438e3f17a799c04e1d2e
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-04-07 17:59:51 -0400

    Format payment due date

--- frontend/src/components/InvoiceSummary.js
+++ frontend/src/components/InvoiceSummary.js
@@ -1,12 +1,18 @@
 import "./InvoiceSummary.css";
 
 const InvoiceSummary = ({ id, paymentDue, total, clientName, status }) => {
+  const dueDate = new Date(paymentDue.replace(/-/g, "/"))
+    .toDateString()
+    .split(" ");
+
   return (
     <ul className="summary">
       <li className="invoice-id">
         #<span className="id-value">{id}</span>
       </li>
-      <li className="payment-due-date">{paymentDue}</li>
+      <li className="payment-due-date">
+        Due {`${dueDate[2]} ${dueDate[1]} ${dueDate[3]}`}
+      </li>
       <li className="payment-total">&pound;{Number(total).toFixed(2)}</li>
       <li className="client-name">{clientName}</li>
       <li
