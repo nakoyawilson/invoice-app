638c519e6969c8c698fb3608e47b8d50283dac89
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-05-28 09:29:20 -0400

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -10,10 +11,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.get("/invoices", (req, res) => {
   res.json(invoices);
 });
@@ -22,6 +19,18 @@ app.get("/invoices/:id", (req, res) => {
   const invoice = invoices.find((invoice) => invoice._id === req.params.id);
   res.json(invoice);
 });
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.listen(
   port,
